17a78958ad75e5caa57b2425c135e9b9d10ba28f
AkshayAgnihotri1090 <sunny.akshay90@gmail.com>
2020-12-21 14:19:32 +0530

    created python file as part of capstone project

---
+++ Capstone_Project.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Notebook created as part of Capstone Project
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
